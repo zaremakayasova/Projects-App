5c05fd9a154fe1ba1ed1295922f95f8d17e33883
Zarema <zaremakayasova@gmail.com>
2021-11-02 12:23:16 -0400

    added media queries to related components, added Loading component to ToursContainer component, finished Tours Project

--- src/components/Tours/Tour/Tour.js
+++ src/components/Tours/Tour/Tour.js
@@ -5,16 +5,21 @@ const Tour = ({ id, name, image, info, price, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
 
   return (
-    <div className='tour'>
-      <img src={image} />
-      <div className='tour-info'>
-      <h2>{name}</h2>
-      <span>$ {price}</span>
-      <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
-      <button className='tour-read-more' onClick={() => setReadMore(!readMore)}>
-        {readMore ? "Show Less" : "Read More"}
-      </button>
-      <button className='tour-remove' onClick={() => removeTour(id)}>Not interested</button>
+    <div className="tour">
+      <img src={image} alt="tour-picture" />
+      <div className="tour-info">
+        <h2>{name}</h2>
+        <span>$ {price}</span>
+        <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
+        <button
+          className="tour-read-more"
+          onClick={() => setReadMore(!readMore)}
+        >
+          {readMore ? "Show Less" : "Read More"}
+        </button>
+        <button className="tour-remove" onClick={() => removeTour(id)}>
+          Not interested
+        </button>
       </div>
     </div>
   );
--- src/components/Tours/ToursContainer/ToursContainer.js
+++ src/components/Tours/ToursContainer/ToursContainer.js
@@ -1,30 +1,44 @@
 import React, { useEffect, useState } from "react";
 import "./ToursContainer.css";
 import Tours from "../Tours/Tours";
+import Loading from "../Loading/Loading";
 
 const ToursContainer = () => {
   const [tours, setTours] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://course-api.com/react-tours-project"
-      );
-      const tours = await response.json();
-      setTours(tours);
-    };
-    fetchData();
-  }, []);
+  const [loading, setLoading] = useState(true);
 
   const removeTour = (id) => {
     const filteredTours = tours.filter((tour) => tour.id !== id);
     setTours(filteredTours);
   };
 
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(
+        "https://course-api.com/react-tours-project"
+      );
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   if (tours.length === 0) {
     return (
-      <div className='tours-container'>
+      <div className="tours-container">
         <h2>No tours left</h2>
+        <button onClick={() => fetchTours()}>Refresh</button>
       </div>
     );
   }
