bb9224f8fd2231544fc276eef6a8ce25d6bdd4a6
Zarema <zaremakayasova@gmail.com>
2021-11-08 15:21:55 -0500

    styled buttons in MenuContainer component

--- src/components/Menu/MenuContainer/MenuContainer.js
+++ src/components/Menu/MenuContainer/MenuContainer.js
@@ -8,10 +8,12 @@ const MenuContainer = () => {
     <div className="menu-container">
       <h1>Our Menu</h1>
       <hr />
-      <button>All</button>
-      <button>Breakfast</button>
-      <button>Lunch</button>
-      <button>Shakes</button>
+      <div className='menu-buttons'>
+        <button>All</button>
+        <button>Breakfast</button>
+        <button>Lunch</button>
+        <button>Shakes</button>
+      </div>
       <div className="menu">
         {menu.map((menuItem) => (
           <MenuItem key={menuItem.id} {...menuItem} />
