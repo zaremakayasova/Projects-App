a24a7643e98e197b2039e1cbe122028dffff3c1d
Zarema <zaremakayasova@gmail.com>
2021-11-10 17:35:03 -0500

    added filter functionality to MenuContainer component, finished Menu project

--- src/components/Menu/MenuContainer/MenuContainer.js
+++ src/components/Menu/MenuContainer/MenuContainer.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MenuContainer.css";
 import menu from "../data";
 import MenuItem from "../MenuItem/MenuItem";
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 
 const MenuContainer = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <div className="menu-container">
       <h1>Our Menu</h1>
       <hr />
       <div className="menu-buttons">
-        <button>All</button>
-        <button>Breakfast</button>
-        <button>Lunch</button>
-        <button>Shakes</button>
+        {categories.map((category, index) => (
+          <button key={index} onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        ))}
       </div>
       <div className="menu">
-        {menu.map((menuItem) => (
+        {menuItems.map((menuItem) => (
           <MenuItem key={menuItem.id} {...menuItem} />
         ))}
       </div>
